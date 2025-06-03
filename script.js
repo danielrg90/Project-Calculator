c82c21758d1393fcfadc5d651cd409d09c8bb762
Daniel R Gonzalez <108524641+danielrg90@users.noreply.github.com>
2025-06-03 02:08:35 -0700

    create functions for basic math operations

---
+++ script.js
@@ -0,0 +1,26 @@
+function add(a, b) { 
+    return a + b; 
+}; 
+
+function subtract(a, b) { 
+    return a - b; 
+}; 
+
+function multiply(a, b) { 
+    return a * b; 
+}; 
+
+function divide(a, b) { 
+    return a / b; 
+}; 
+
+const firstNumber = 0; 
+
+const secondNumber = 0; 
+
+const operator = ""; 
+
+function operate(num1, num2) { 
+    return num1 + num2; 
+}; 
+
