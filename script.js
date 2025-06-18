3d9bf51277cae430f3ce3200e1d8f0402443588b
Daniel R Gonzalez <108524641+danielrg90@users.noreply.github.com>
2025-06-18 12:30:37 -0700

    add click event for button display

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function add(a, b) { 
     return a + b; 
 }; 
@@ -13,7 +12,7 @@ function multiply(a, b) {
 
 function divide(a, b) { 
     if (b === 0) { 
-        return "ERROR" }
+        return "whoopsies" }
         else { 
             return a / b; 
         }; 
@@ -22,9 +21,13 @@ function divide(a, b) {
 function operate() {
     let results = 0;  
     if (operator === "+") {
-        results += num1 - num2
-    } else { 
         results += +num1 + +num2
+    } else if (operator === "-") { 
+        results += num1 - num2
+    } else if (operator === "*") { 
+        results += num1 * num2
+    } else if (operator === "/") { 
+        results += num1 / num2
     }
     return display.textContent = results;   
      
@@ -34,30 +37,13 @@ let num1;
 
 let num2; 
 
-let operator; 
-  
+let operator;  
 
-function displayNumber() {   
-}; 
+const display = document.querySelector(".display")
 
-const display = document.querySelector(".display"); 
-
-const one = document.querySelector(".one"); 
-one.addEventListener("click", () => {
-    display.textContent = one.value; 
-    num1 = one.value;   
+const btns = document.querySelectorAll("button"); 
+btns.forEach(button => {
+    button.addEventListener("click", () => {
+        display.textContent = button.value; 
+    }); 
 }); 
-
-const two = document.querySelector(".two"); 
-two.addEventListener("click", () => {
-    display.textContent = two.value; 
-    num2 = two.value; 
-}); 
-
-const plus = document.querySelector(".plus"); 
-plus.addEventListener("click", () => {
-    operator = "+";  
-}); 
-
-const equals = document.querySelector(".equals"); 
-equals.addEventListener("click", operate);  
